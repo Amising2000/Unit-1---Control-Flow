02429afcf3f0f51d4be5d5e79d819766a91b1a7a
Angad <angadveerasija@gmail.com>
2025-09-19 08:57:22 -0400

    lesson 2

---
+++ Lesson2 - conditionals practice/practice.py
@@ -0,0 +1,9 @@
+age = (input("Enter your age"))
+rating = input("Enter movie rating")
+rating_values = "P", "PG", "PG-13", "R"
+
+if age:
+    int(age)
+    if 0 < age < 13 and 
+else:
+    print("Please enter a valid age")
