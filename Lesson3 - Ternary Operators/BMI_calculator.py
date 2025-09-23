baa2f704e98b37eec72792dd0628b1a5183cab07
Angad <angadveerasija@gmail.com>
2025-09-23 13:32:26 -0400

    BMI edits

--- Lesson3 - Ternary Operators/BMI_calculator.py
+++ Lesson3 - Ternary Operators/BMI_calculator.py
@@ -1,12 +1,15 @@
 weight = float(input("Enter your weight (in pounds): "))
 height = float(input("Enter your height (in inches): "))
 
+if height or weight < 0:
+    print ("Error, please enter a valid weight or height.")
+
 BMI = (weight / (height * height)) * 703
 
-category = ("Underweight" if BMI < 18.5 else
-            "Normal" if 18.5 <= BMI <= 25 else
-            "Overweight" if 25 <= BMI < 30 else
+category = ("Underweight- Gain weight" if BMI < 18.5 else
+            "Normal- Maintain weight" if 18.5 <= BMI <= 25 else
+            "Overweight- Lose weight" if 25 <= BMI < 30 else
             "Obese"
             )
 
-print(f"BMI: {category}")
+print(f"Weight: {weight}lb\nHeight: {height}in\nBMI: {category}")
